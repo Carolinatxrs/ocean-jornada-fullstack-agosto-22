dea13c2a5f35f3bec04daa139f83fefd9f44b7ec
Carolina Teixeira <carolina.txrs@gmail.com>
2022-08-09 12:49:29 -0400

    Express instalado

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+//cria aplicação utilizando o express
+const express = require('express')	
+const app = express()
+
+//cria o endpoint principal
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+//aplicação ouvindo na porta
+app.listen(3000)
