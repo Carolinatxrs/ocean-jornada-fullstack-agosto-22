7fc8bb42c64733066d925b8b64ead207cb458689
Carolina Teixeira <carolina.txrs@gmail.com>
2022-08-11 10:09:31 -0400

    Criação da lista de objetos

--- backend/index.js
+++ backend/index.js
@@ -11,5 +11,25 @@ app.get("/oi", function (req, res) {
   res.send("Olá, mundo!");
 });
 
+//Backend armazena a pontuação das jogadas
+//Criar a lista com as pontuações
+const lista = [
+  {
+    id: 1,
+    nome: "Paulo",
+    pontos: 90,
+  },
+  {
+    id: 2,
+    nome: "Daniel",
+    pontos: 52,
+  },
+  {
+    id: 3,
+    nome: "Carolina",
+    pontos: 97,
+  },
+];
+
 //aplicação ouvindo na porta
 app.listen(3000)
